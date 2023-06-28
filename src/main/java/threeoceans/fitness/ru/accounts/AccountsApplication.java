13cc329643f1d0e635b51bade4ff84bbaca72529
Kirill <kiruxin16@gmail.com>
2023-06-28 22:14:05 +0700

    Initial commit

---
+++ src/main/java/threeoceans/fitness/ru/accounts/AccountsApplication.java
@@ -0,0 +1,13 @@
+package threeoceans.fitness.ru.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
